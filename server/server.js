f30d4f2d902379835385f71a1c00b5cdf0d61721
Rebecca Silus <rebeccasilus@gmail.com>
2018-11-19 11:34:57 -0600

    add environment variable port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require( 'body-parser' );
 app.use( express.static( './server/public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 // globals
-const port = 5000;
+const PORT = process.env.PORT || 5000; // write PORT like like this for now on // PORT must be caps here
 let history = [];
 // spin up server
 app.listen( port, ()=>{
